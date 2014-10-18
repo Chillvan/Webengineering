ee825a40fe3cf9d06bc6a44cce38b1b3b4ecb1bb
Chillvan <silvan.hoppler@gmail.com>
2014-10-18 09:57:27 +0200

    Datenbankanbindung Mieter

--- mieter.php
+++ mieter.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 <html>
 <head>
 <link rel="stylesheet" href="mycss.css" type="text/css">
@@ -14,6 +11,25 @@
 	<a href = "abrechnung.php">Abrechnung</a>
 	<a href = "nebenabrechnung.php">Heizkosten- und Nebenkostenabrechnung</a>
 </p>
+
+<?php
+$user = 'u566874539_admin';
+$pass = 'WEFHNW14';
+try {
+    $dbh = new PDO('mysql:host=mysql.hostinger.de;dbname=u566874539_ftw', $user, $pass);
+    foreach ($dbh->query('SELECT * from Mieterspiegel') as $row) {
+    print_r("Mieternummer: ".$row['Mieternummer']." | Wohnungsnummer: ".$row['Wohnungsnummer'].
+            " | Name: ".$row['Name']." | Vorname: ".$row['Vorname'].
+            " | Mietzins: ".$row['Mietzins']."%"." | Rechnungsadresse: ".$row['Rechnungsadresse'].
+            " | Aktiv: ".$row['Aktiv']."<br/>");
+   }
+   $dbh = null;
+} catch (PDOException $e) {
+   print "Error!: " . $e->getMessage() . "<br/>";
+   die();
+}
+
+?>
 
 </body>
 </html>
