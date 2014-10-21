b204f8620196eed4a0dd65aaf06dd00014700ead
bendltd <bendltd@bastians-mbp.edu.ds.fhnw.ch>
2014-10-21 10:44:26 +0200

    Header

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
-<?php
-
-?>
 <html>
 <head>
-<title>Mietverwaltung 12-Familienhaus</title>
-<style type="text/css">
-h1 {font-family: Arial; font-size: 32; text-align: center; color: red}
-p.nav {font-family: Arial; font-size: 14; text-align: right}
-p.haupt {font-family: Arial; font-size: 16; text-align: left}
-</style>
+    <link rel="stylesheet" href="mycss.css" type="text/css">
+    <title>Mietverwaltung 12-Familienhaus</title>
 </head>
 <body>
+    <header>
+        <div class="inner">
+            <a href = "mieter.php">Mieter</a>
+            <a href = "rechnungen.php">Rechnungen</a>
+            <a href = "abrechnung.php">Abrechnung</a>
+            <a href = "nebenabrechnung.php">Heizkosten- und Nebenkostenabrechnung</a>
+        </div>
+    </header>
 <h1>Hauptseite</h1><br/>
 <p class = "haupt">
 	<a href = "mieter.php">Mieter</a><br/>
