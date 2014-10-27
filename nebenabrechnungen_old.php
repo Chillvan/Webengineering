42aa2b962064b2cb51eaaa5b33c1c501c00d3de9
Chillvan <silvan.hoppler@gmail.com>
2014-10-27 10:49:38 +0100

    OLD

---
+++ nebenabrechnungen_old.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="mycss.css" type="text/css">
+        <title>Heizkosten- und Nebenkostenabrechnung</title>
+    </head>
+    <body>
+    <h1>Heizkosten- und Nebenkostenabrechnung</h1>
+    <ul>
+        <li><a class="aheader" href = "mieter.php">//Mieter</a></li>
+        <li><a class="aheader" href = "rechnungen.php">//Rechnungen</a></li>
+        <li><a class="aheader" href = "abrechnung.php">//Abrechnung</a></li>
+        <li><a class="aheader" href = "nebenabrechnungen.php">//Heizkosten- und Nebenkostenabrechnung</a></li>
+        <li id="line">&nbsp;</li>
+    </ul>
+    </body>
+</html>
