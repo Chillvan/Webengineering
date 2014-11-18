0465f7232538d08a6b1be694240c84123cda7fff
bendltd <bendltd@192.168.0.16>
2014-11-18 14:48:40 +0100

    fpdf new try

--- modal.php
+++ modal.php
@@ -292,7 +292,7 @@ class modal {
                 <form class="form" action="pdfDrucken.php" method="post">
                   <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                    <h4 class="modal-title">Gesamtabrechnung als PDF drucken</h4>
+                    <h4 class="modal-title">PDF drucken</h4>
                   </div>
                   <div class="modal-body">
                     <p>Sind Sie sicher, dass Sie die Gesamtabrechnung als PDF drucken wollen?</p>
